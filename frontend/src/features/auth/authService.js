9830229724ce53cf6e72501659b1919d8a6179bf
sershishkov <ser.shishkov>
2022-03-01 18:38:01 +0200

    auth done

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -6,35 +6,46 @@ const API_URL = '/api/auth';
 const register = async (userData) => {
   const responseToken = await axios.post(`${API_URL}/register`, userData);
   const token = responseToken.data.token;
+  console.log('token', token);
 
   if (token) {
     localStorage.setItem('token', JSON.stringify(token));
   }
+
   const config = {
     headers: {
       Authorization: `Bearer ${token}`,
     },
   };
+
   const responseUser = await axios.get(`${API_URL}/me`, config);
 
   const user = responseUser.data;
-  console.log(user);
-  console.log(token);
   return user ? user : null;
 };
 
 const login = async (userData) => {
-  const response = await axios.post(`${API_URL}/login`, userData);
+  const responseToken = await axios.post(`${API_URL}/login`, userData);
+  const token = responseToken.data.token;
 
-  if (response.data) {
-    localStorage.setItem('user', JSON.stringify(response.data));
+  if (token) {
+    localStorage.setItem('token', JSON.stringify(token));
   }
 
-  return response.data;
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const responseUser = await axios.get(`${API_URL}/me`, config);
+
+  const user = responseUser.data;
+  return user ? user : null;
 };
 
 const logout = async () => {
-  localStorage.removeItem('user');
+  localStorage.removeItem('token');
 };
 
 const authService = {
